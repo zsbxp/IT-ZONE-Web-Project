dcb80018ca44e5ee1d69c057e78af17610e42ab6
Zeng Shao Bin <249505968@qq.com>
2011-07-14 15:20:58 +0800

    big Change!

---
+++ web/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ROR::Application.config.secret_token = '382d9f74d5822cfb7804950a8b2404377e922faae6e79a27b581182ba9d210da1e71b227221ac51840b49a2de561c2b99a39be31e32b8a691274aff4bc8438ac'
