bc71ded1425fa2c692b521a5feaaf2892b3b46ba
Zeng Shao Bin <249505968@qq.com>
2011-07-14 14:37:43 +0800

    First push！

---
+++ HiGit/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ROR::Application.config.secret_token = 'b6974525058fb8114fa341bcac30da88bca9c31987715adfa71b0c7830a5b77ae989f3861997923679f4b240f8a428fb35c3f0fae412c4364c99bf5b3f86171e'
